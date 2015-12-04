2ece67fbf325988b3cc03824975254502ea2cd7e
efmorris <efmorris@ucsc.edu>
2015-12-03 16:08:05 -0800

    cat table

--- finalProject/models/tables.py
+++ finalProject/models/tables.py
@@ -16,11 +16,18 @@
 #########################################################################
 from datetime import datetime
 
-db.define_table('cats',
-                Field('name', 'text'),
-                Field('fromDB', 'text', default=True),
-                Field('cat_creator', 'text'),
-                Field('breed', 'text'),
-                Field('stateLoc', 'text'),
-                Field('draft_id', 'text')
-            )
+cat_breeds = ['Siamese', 'Persian', 'British Shorthair', 'Abyssinian', 'Maine Coon', 'American Shorthair', 'Bengal', 'Sphynx', 'Ragdoll', 'Exotic Shorthair', 'Burmese', 'Scottish Fold', 'Birman', 'Siberian','Russian Blue', 'Norwegian Forrest', 'Himalayan', 'Turkish Angora', 'Munchkin']
+states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY','LA', 'ME', 'ME','MD','MA','MI','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+
+db.define_table('cat',
+                Field('Name'),
+                Field('Breed', requires=IS_IN_SET(cat_breeds)),
+                Field('Bio', 'text'),
+                Field('Age', 'integer'),
+                Field('place', requires=IS_IN_SET(states)),
+                Field('Price', 'integer'),
+                Field('rating', readable =False, writable=False),
+                Field('image', 'upload'),
+                Field('human', db.auth_user, default=auth.user_id, readable =False, writable=False)
+                )
